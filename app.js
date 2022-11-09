3a2b43ef11082c1a394e01ffa55285f716734532
taimoorbaig99 <muhammadtaimoorbaig64@gmail.com>
2022-11-10 01:02:31 +0400

    javascriptarrayass

---
+++ app.js
@@ -0,0 +1,34 @@
+var a=["a","b", "c" ,"d"]
+
+// a.push("f");
+// a.pop();
+// a.shift("a");
+// a.unshift("u");
+// var b= a.slice(0,2)
+// var b = a.splice(0,3);
+// console.log(b);
+// console.log(a);
+
+// var b=a.indexOf("b");
+// console.log(b);
+
+
+
+
+// var b= a.indexOf("c");
+// console.log(b)
+
+// var g=a.length;
+// console.log(g);
+
+
+for(b=0; b<10; b++ ){
+alert(b);
+console.log("Taimoor");
+console.log(b);
+}
+
+
+
+
+     
